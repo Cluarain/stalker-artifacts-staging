1dfbd08b791743ff7fed451e753435a439dba3cd
Cluarain <maxohezin@mail.ru>
2026-02-04 21:55:24 +0500

    path fix

---
+++ out/_next/static/LBOSLXAFDMturrmQBCekN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/stalker-artifacts-staging//_next/:path+",
+        "destination": "/stalker-artifacts-staging/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
